bd31660daf8970600f08c03f4d0cd1f733f3a13b
p4u <pau@dabax.net>
2021-02-04 20:04:43 +0100

    update multirpc
    Signed-off-by: p4u <pau@dabax.net>

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.15
 require (
 	github.com/arnaucube/go-blindsecp256k1 v0.0.0-20210201183224-d09769d51033
 	github.com/spf13/pflag v1.0.5
-	github.com/vocdoni/multirpc v0.1.10
+	github.com/vocdoni/multirpc v0.1.12
 	go.vocdoni.io/dvote v0.6.1-0.20210130094936-75dbb92de3f0
 )
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ "$1" == "blind" ] && {
 echo "=> ECDSA blind signatre"
-R="$(curl -s 127.0.0.1:5000/ca -X POST -d '{"request":{"method":"auth", "signatureType":"ECDSA_BLIND", "request":"1234"}, "id":"1234"}' | jq -Mc .request.token)"
+R="$(curl -s 127.0.0.1:5000/ca -X POST -d '{"request":{"method":"auth", "signatureType":"ECDSA_BLIND", "request":"1234"}, "id":"1234"}' | jq -Mc .response.token)"
 [ "$R" == "" ] && { echo "Error receiving signerR"; exit 1; }
 hash="$(echo $RANDOM | sha256sum | awk '{print $1}')"
 curl -s 127.0.0.1:5000/ca -X POST -d  '{"request":{"method":"sign", "request":"1234", "signatureType":"ECDSA_BLIND", "token":'$R', "messageHash":"'$hash'"},"id":"1234"}' | jq .
@@ -9,7 +9,7 @@ exit 0
 }
 
 echo "=> ECDSA signatre"
-R="$(curl -s 127.0.0.1:5000/ca -X POST -d '{"request":{"method":"auth", "signatureType":"ECDSA", "request":"1234"}, "id":"1234"}' | jq -Mc .request.token)"
+R="$(curl -s 127.0.0.1:5000/ca -X POST -d '{"request":{"method":"auth", "signatureType":"ECDSA", "request":"1234"}, "id":"1234"}' | jq -Mc .response.token)"
 [ "$R" == "" ] && { echo "Error receiving signerR"; exit 1; }
 hash="$(echo $RANDOM | sha256sum | awk '{print $1}')"
 curl -s 127.0.0.1:5000/ca -X POST -d  '{"request":{"method":"sign", "request":"1234", "signatureType":"ECDSA", "token":'$R', "message":"'$hash'"},"id":"1234"}' | jq .
