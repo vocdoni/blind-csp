fe1ed19a05161318e20a3452cd984447fbda4ff7
Jør∂¡ <4456749+brickpop@users.noreply.github.com>
2021-12-23 13:32:05 +0100

    adding an RSA signature handler (#10)

---
+++ handlers/rsahandler/rsa.go
@@ -0,0 +1,210 @@
+package rsahandler
+
+import (
+	"bytes"
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/vocdoni/blind-csp/csp"
+	"go.vocdoni.io/dvote/db"
+	"go.vocdoni.io/dvote/db/metadb"
+	"go.vocdoni.io/dvote/log"
+)
+
+const (
+	ELECTION_ID_STR_LENGTH      = 64
+	VOTER_ID_STR_LENGTH         = 64
+	SIGNED_MESSAGE_BYTES_LENGTH = 64
+)
+
+// RsaHandler is a handler that allows only 1 registration for IP
+type RsaHandler struct {
+	kv        db.Database
+	keysLock  sync.RWMutex
+	rsaPubKey *rsa.PublicKey
+}
+
+func (rh *RsaHandler) addKey(index, value []byte) error {
+	rh.keysLock.Lock()
+	defer rh.keysLock.Unlock()
+	tx := rh.kv.WriteTx()
+	defer tx.Discard()
+	if err := tx.Set(index, value); err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+func (rh *RsaHandler) exist(index []byte) bool {
+	rh.keysLock.RLock()
+	defer rh.keysLock.RUnlock()
+	tx := rh.kv.WriteTx()
+	defer tx.Discard()
+	_, err := tx.Get(index)
+	return err == nil
+}
+
+// GetName returns the name of the handler
+func (rh *RsaHandler) GetName() string {
+	return "rsa"
+}
+
+// Init initializes the handler.
+// Takes one argument for persistent data directory.
+func (rh *RsaHandler) Init(opts ...string) (err error) {
+	if len(opts) != 2 {
+		return fmt.Errorf("rsa handler requires a file path with the validation RSA key")
+	}
+
+	rh.kv, err = metadb.New(db.TypePebble, filepath.Clean(opts[0]))
+	if err != nil {
+		return err
+	}
+
+	pubKeyBytes, err := os.ReadFile(opts[1])
+	if err != nil {
+		return err
+	}
+
+	rh.rsaPubKey, err = parseRsaPublicKey(string(pubKeyBytes))
+	return err
+}
+
+// Auth is the handler for the rsa handler
+func (rh *RsaHandler) Auth(r *http.Request,
+	ca *csp.Message, pid []byte, st string) (bool, string) {
+	authData, err := parseRsaAuthData(ca.AuthData)
+	if err != nil {
+		return false, err.Error()
+	}
+	if !bytes.Equal(pid, authData.ProcessId) {
+		return false, "the electionId does not match the URL one"
+	}
+
+	// Verify signature
+	if err := validateRsaSignature(authData.Signature, authData.Message, rh.rsaPubKey); err != nil {
+		return false, "invalid signature"
+	}
+
+	if st == csp.SignatureTypeSharedKey {
+		return true, "please, do not share the key"
+	}
+	if rh.exist(authData.VoterId) {
+		return false, "already registered"
+	}
+
+	err = rh.addKey(authData.VoterId, nil)
+	if err != nil {
+		return false, "could not add key"
+	}
+	log.Infof("new user registered with id %x", authData.VoterId)
+
+	return true, ""
+}
+
+// RequireCertificate must return true if the auth handler requires some kind of client
+// TLS certificate. If true then CertificateCheck() and HardcodedCertificate() methods
+// must be correctly implemented. Else both function can just return true and nil.
+func (rh *RsaHandler) RequireCertificate() bool {
+	return false
+}
+
+// CertificateCheck is used by the Auth handler to ensure a specific certificate is
+// added to the CA cert pool on the HTTP/TLS layer (optional).
+func (rh *RsaHandler) CertificateCheck(subject []byte) bool {
+	return true
+}
+
+// Certificates returns a hardcoded CA certificated that will be added to the
+// CA cert pool by the handler (optional).
+func (rh *RsaHandler) Certificates() [][]byte {
+	return nil
+}
+
+// Internal data handlers
+
+func parseRsaPublicKey(pubKey string) (*rsa.PublicKey, error) {
+	block, rest := pem.Decode([]byte(pubKey))
+	if len(rest) > 0 {
+		return nil, fmt.Errorf("failed to parse the public key")
+	}
+
+	parsedKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	switch parsedKey.(type) {
+	case *rsa.PublicKey:
+		break
+	default:
+		return nil, fmt.Errorf("cannot parse the public key")
+	}
+	return parsedKey.(*rsa.PublicKey), nil
+}
+
+type rsaAuthData struct {
+	ProcessId []byte
+	VoterId   []byte
+	Message   []byte
+	Signature []byte
+}
+
+// parseRsaAuthData transforms the incoming authData string array and returns a digested output
+// of the relevant parameters for the handler
+func parseRsaAuthData(authData []string) (*rsaAuthData, error) {
+	if len(authData) != 3 {
+		return nil, fmt.Errorf("invalid params (3 items expected)")
+	}
+
+	// Catenate hex
+	processId := authData[0]
+	if len(authData[0]) != ELECTION_ID_STR_LENGTH {
+		return nil, fmt.Errorf("invalid electionId")
+	}
+	processIdBytes, err := hex.DecodeString(authData[0])
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode processId")
+	}
+	voterId := authData[1]
+	if len(voterId) != VOTER_ID_STR_LENGTH {
+		return nil, fmt.Errorf("invalid voterId")
+	}
+	voterIdBytes, err := hex.DecodeString(voterId)
+	if err != nil || len(voterIdBytes) != VOTER_ID_STR_LENGTH/2 {
+		return nil, fmt.Errorf("invalid voterId: %w", err)
+	}
+	message, err := hex.DecodeString(processId + voterId)
+	if err != nil || len(message) != SIGNED_MESSAGE_BYTES_LENGTH {
+		// By discard, only processId can be invalid
+		return nil, fmt.Errorf("invalid electionId: %w", err)
+	}
+	signature, err := hex.DecodeString(authData[2])
+	if err != nil || len(signature) == 0 {
+		return nil, fmt.Errorf("invalid voterId")
+	}
+
+	return &rsaAuthData{
+		ProcessId: processIdBytes,
+		VoterId:   voterIdBytes,
+		Message:   message,
+		Signature: signature,
+	}, nil
+}
+
+// validateRsaSignature hashes the given message and verifies the signature against
+// the given public key
+func validateRsaSignature(signature []byte, message []byte, rsaPublicKey *rsa.PublicKey) error {
+	msgHash := sha256.Sum256(message)
+
+	return rsa.VerifyPKCS1v15(rsaPublicKey, crypto.SHA256, msgHash[:], signature)
+}
---
+++ handlers/rsahandler/rsa_test.go
@@ -0,0 +1,156 @@
+package rsahandler
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+)
+
+const (
+	processId = "88e66cdf1cac93ded9c8d13b7cc74601c7f25bf50d392549e1146eaa8429ab01"
+	voterId   = "51bc804fdb2122c0a8b221bf5b3683395151f30ac6e86d014bb38854eff483de"
+	signature = "39143730467aefbfbdefbfbdefbfbdefbfbd77efbfbdefbfbdefbfbdefbfbd54" +
+		"757eefbfbd69efbfbd5709efbfbd1e65efbfbd71efbfbdefbfbd55efbfbd1e0eefbfbdefbf" +
+		"bdefbfbd603941efbfbd686cefbfbdefbfbd49470cefbfbdefbfbd0befbfbd48efbfbd20ef" +
+		"bfbd48344425efbfbdefbfbdefbfbd6c184058efbfbdefbfbdefbfbd45efbfbdefbfbd133e" +
+		"efbfbd1731724a2829dcb26fefbfbdefbfbd1125efbfbdefbfbd77efbfbdefbfbd49efbfbd" +
+		"56efbfbd05efbfbdefbfbdefbfbd77efbfbdefbfbd2e64efbfbdefbfbdefbfbdefbfbdefbf" +
+		"bdefbfbdefbfbdefbfbd3f431fefbfbdefbfbdefbfbd241e42efbfbdefbfbdefbfbd35efbf" +
+		"bd555ec3bbd9bc59efbfbdefbfbdefbfbd4cefbfbdefbfbd58e1889defbfbdefbfbd2b13ef" +
+		"bfbdefbfbdefbfbdefbfbd4b45181d78efbfbdefbfbddbabefbfbd7e18efbfbd7fefbfbdef" +
+		"bfbd3cefbfbdefbfbd21efbfbd61efbfbd12efbfbd39efbfbd0aefbfbdefbfbd42417b586f" +
+		"efbfbdefbfbd0b2d6322efbfbd3fefbfbd1a0213efbfbd0653efbfbd78efbfbdefbfbd58ef" +
+		"bfbd2a69c585efbfbdefbfbd61276aefbfbdefbfbd2defbfbdefbfbd091321efbfbdefbfbd" +
+		"efbfbdefbfbdefbfbd0638efbfbd612341efbfbdefbfbd183eefbfbdefbfbdefbfbdefbfbd281e13"
+	rsaPubKey = `-----BEGIN PUBLIC KEY-----
+MIIBIDANBgkqhkiG9w0BAQEFAAOCAQ0AMIIBCAKCAQEApc2hU8zulyJzdQE5IPAv
+B2BgveoZmYUmPEjSb4DViBoATK1hlaY8Psp5vj0H0L4tM8AlXRhPQlECibhgccig
+xQFcG7CLXiSAn7c4XoR+J2SCgx76Fwl9L3WhQigxyKsmpGIqubseydmwfJi4TBnq
+qnX4prsW1PT8GpG35t8Qi8PtkXVGmL7G5pkPXtF0hRzKSfhzsDBbJsl6Jk/Rn5Id
+pKHXL22FdbE9fGzIlW2a6Zdd0b0Q3FZBMnWLSwo0OwBtC/qNnDTCzboig9djiFmA
+yuj8jVhsy050nI72TAONjGKi+xn4lYfdOV2k6TyvpRHfylHouK2v0/bktSlkFI0y
+nwIBAw==
+-----END PUBLIC KEY-----`
+)
+
+func TestPublicKey(t *testing.T) {
+	pubK, err := parseRsaPublicKey(rsaPubKey)
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, pubK, qt.IsNotNil)
+}
+
+func TestAuthDataParserErr(t *testing.T) {
+	inputVectors := [][]string{
+		{"", "", ""},
+		{"", ""},
+		{""},
+		{"___66cdf1cac93ded9c8d13b7cc74601c7f25bf50d392549e1146eaa8429ab01", voterId, signature},
+		{processId, "___66cdf1cac93ded9c8d13b7cc74601c7f25bf50d392549e1146eaa8429ab01", signature},
+		{processId, voterId, "___66cdf1cac93ded9c8d13b7cc74601c7f25bf50d392549e1146eaa8429ab01"},
+		{"1234", "1234", "1234"},
+	}
+
+	for _, input := range inputVectors {
+		res, err := parseRsaAuthData(input)
+		qt.Assert(t, res, qt.IsNil)
+		qt.Assert(t, err, qt.IsNotNil)
+	}
+}
+
+func TestAuthDataParser(t *testing.T) {
+	res, err := parseRsaAuthData([]string{
+		processId,
+		voterId,
+		signature,
+	})
+
+	qt.Assert(t, res.VoterId, qt.IsNotNil)
+	qt.Assert(t, res.Message, qt.IsNotNil)
+	qt.Assert(t, res.Signature, qt.IsNotNil)
+	qt.Assert(t, err, qt.IsNil)
+
+	aa := hex.EncodeToString(res.VoterId)
+	qt.Assert(t, aa, qt.Equals, voterId)
+
+	bb := hex.EncodeToString(res.Message)
+	qt.Assert(t, bb, qt.Equals, processId+voterId)
+
+	cc := hex.EncodeToString(res.Signature)
+	qt.Assert(t, cc, qt.Equals, signature)
+}
+
+// func TestSignature1(t *testing.T) {
+// 	res, err := parseRsaAuthData([]string{
+// 		processId,
+// 		voterId,
+// 		signature,
+// 	})
+// 	qt.Assert(t, err, qt.IsNil)
+
+// 	pubK, _ := parseRsaPublicKey()
+// 	err = validateRsaSignature(res.Signature, res.Message, pubK)
+
+// 	qt.Assert(t, err, qt.IsNil)
+// }
+
+func TestSignature2(t *testing.T) {
+	// Manual inputs verified on cyberchef
+	message := []byte("hello")
+	sig, err := hex.DecodeString(
+		"6ff990e9522a18c5ec75576ac2c3477ae2f85c3e51ac659b37fd87ea1e35955c" +
+			"5b5a72af46dc80224968001369c5c022ae3ae304bef4e6ba992685881b5290c6" +
+			"84cd25cc2c694215abf79609d049260fe2bf01e54be0e7ceecbe5e31be95fc86" +
+			"78877d634f3577bde74d09074348fa6aad90a8449defb12fda3136b8eeb58148",
+	)
+	qt.Assert(t, err, qt.IsNil)
+	pubKstr := `-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHBhoIO6WsbQR6Dr+fyzwdUfrqz4
+G1s4fKvcQR1NqfvGchXHTZZply7P+1NZnO4UX8z7T9VoMRSoS7lM8jdIeOjoyZuk
+0WmNHZXGFeDNhoWtX/IZwy7z/e4qUD+rt1xVU3jjJqkQBSyar1FB+x9tG2qMGPhC
+4cKjDWyJtRlopwbtAgMBAAE=
+-----END PUBLIC KEY-----`
+
+	// Using the first one available so far
+	block, _ := pem.Decode([]byte(pubKstr))
+
+	parsedKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	qt.Assert(t, err, qt.IsNil)
+
+	qt.Assert(t,
+		validateRsaSignature(sig, message, parsedKey.(*rsa.PublicKey)),
+		qt.IsNil,
+	)
+}
+
+func TestSignature3(t *testing.T) {
+	// Manual inputs verified with openssl
+	message, _ := hex.DecodeString("88e66cdf1cac93ded9c8d13b7cc74601c7f25bf50d392549e1146eaa8429ab01" +
+		"51bc804fdb2122c0a8b221bf5b3683395151f30ac6e86d014bb38854eff483de")
+	sig, err := hex.DecodeString("538cd5175e9b03f01dcb7ec725202a268fbbb60355b570c61938e46a5c6de882" +
+		"0d3a567402e785cdc70251c98c2671d9c02a90cafd8b510e2241f978d3ee4c07" +
+		"dc1b67c7fd2313baf1e50a2655ae6c88aa61a4e31243854f8519abfb7c70c33b" +
+		"a0048a34660a8e93d37449b5ed93ef61291ff797e250409ba53119bc4e731f17",
+	)
+	qt.Assert(t, err, qt.IsNil)
+	pubKstr := `-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHBhoIO6WsbQR6Dr+fyzwdUfrqz4
+G1s4fKvcQR1NqfvGchXHTZZply7P+1NZnO4UX8z7T9VoMRSoS7lM8jdIeOjoyZuk
+0WmNHZXGFeDNhoWtX/IZwy7z/e4qUD+rt1xVU3jjJqkQBSyar1FB+x9tG2qMGPhC
+4cKjDWyJtRlopwbtAgMBAAE=
+-----END PUBLIC KEY-----`
+
+	// Using the first one available so far
+	block, _ := pem.Decode([]byte(pubKstr))
+
+	parsedKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	qt.Assert(t, err, qt.IsNil)
+
+	qt.Assert(t,
+		validateRsaSignature(sig, message, parsedKey.(*rsa.PublicKey)),
+		qt.IsNil,
+	)
+}
