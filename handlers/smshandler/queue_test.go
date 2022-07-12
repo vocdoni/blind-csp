7d8eeb0c615f781b3629f1ca0c3aa3efde6fa678
Gui Iribarren <gui@altermundi.net>
2022-07-12 09:20:40 -0300

    fixup! queue_test.go: WIP, never fails as go test but ATM useful for playing

--- handlers/smshandler/queue_test.go
+++ handlers/smshandler/queue_test.go
@@ -13,41 +13,53 @@ import (
 	"go.vocdoni.io/dvote/log"
 )
 
+const testIterations = 20
+const smsThrottleTime = DefaultSMSthrottleTime
+
 func TestSmsQueue(t *testing.T) {
 	log.Init("debug", "stderr")
 	rand.Seed(time.Now().UnixNano())
 	smsQueue := newSmsQueue(
 		1*time.Second, // smsCoolDownTime
-		sendChallengeOnceIn2,
+		sendChallengeOnceIn10,
 	)
 	go smsQueue.run()
-	go smsQueueController(smsQueue.response)
-	var userID types.HexBytes = []byte{0xff}
 	var electionID types.HexBytes = []byte{0xee}
-	for i := 0; i < 10; i++ {
-		phone := randomPhone()
-		err := smsQueue.add(userID, electionID, phone, 1234)
+	for i := 0; i < testIterations; i++ {
+		err := smsQueue.add(mockUser(i), electionID, mockPhone(i), 1234)
 		qt.Check(t, err, qt.IsNil)
-		time.Sleep(time.Second)
+		//time.Sleep(time.Second) // wait a bit between each mock sms attempt
 	}
+	smsQueueController(smsQueue.response)
 }
 
 // smsQueueController was copy-pasted from smshandler.go
+// but instead of leaving it running as a goroutine, wrap in a loop of known iterations
 func smsQueueController(ch <-chan (smsQueueResponse)) {
-	for {
+	for i := 0; i < testIterations; i++ {
 		r := <-ch
 		if r.success {
 			log.Infof("challenge successfully sent to %s", r.userID)
 		} else {
 			log.Infof("challenge sending failed for %s", r.userID)
 		}
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(smsThrottleTime)
 	}
+}
+
+func mockUser(i int) types.HexBytes {
+	var userID types.HexBytes = []byte{0xfe, 0xde, byte(i)}
+	return userID
 }
 
 // randomPhone returns a random phonenumber between +34722000000 and +34722999999
 func randomPhone() *phonenumbers.PhoneNumber {
-	n := 722000000 + rand.Intn(999999)
+	return mockPhone(rand.Intn(999999))
+}
+
+// mockPhone returns a phonenumber between +34722000000 and +34722999999
+func mockPhone(i int) *phonenumbers.PhoneNumber {
+	n := 722000000 + i
 	ph, _ := phonenumbers.Parse(fmt.Sprintf("%d", n), "ES")
 	return ph
 }
@@ -62,4 +74,8 @@ func sendChallengeOnceIn(i int) error {
 
 func sendChallengeOnceIn2(phone *phonenumbers.PhoneNumber, challenge int) error {
 	return sendChallengeOnceIn(2)
+}
+
+func sendChallengeOnceIn10(phone *phonenumbers.PhoneNumber, challenge int) error {
+	return sendChallengeOnceIn(10)
 }
